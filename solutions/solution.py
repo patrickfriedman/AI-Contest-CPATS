b5f14cc7796c31481a22c33ba414866150cb0807
patrickfriedman <patrick.s.friedman@gmail.com>
2023-09-10 20:54:16 -0500

    added unit_errors file

--- solutions/solution.py
+++ solutions/solution.py
@@ -6,4 +6,6 @@ def solution(a: list[int]) -> int:
             east_cars += 1
         else:
             passing_cars += east_cars
+            if passing_cars > 1000000000:
+                return -1
     return passing_cars
